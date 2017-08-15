a7fb87387f5c631dd2fb3888fef9d8058ceec0bf
Hamish Coleman <hamish@zot.org>
2017-08-15 13:36:54 +0800

    Improve the NAL packet processing speed enough to be usable

--- lib/MPEG/NAL.pm
+++ lib/MPEG/NAL.pm
@@ -57,4 +57,44 @@ sub peek_type {
     return $type;
 }
 
+# A much trickier resync function - this makes the speed actually usable
+sub resync {
+    my $self = shift;
+
+    # read in bigger chunks to speed things up
+    my $read_size = 4096;
+
+    my $remainder;
+    while(1) {
+        my $buf = $self->read_bytes($read_size);
+        if (!defined($buf)) {
+            return undef;
+        }
+
+        # add in any remainder from the last block
+        if (defined($remainder)) {
+            $buf = $remainder . $buf;
+        }
+
+        my $index = index($buf,"\000\000\000\001");
+        if ($index != -1) {
+            my $buf_len = length($buf);
+            my $actualoffset = -($buf_len-$index);
+            $self->{_fh}->seek($actualoffset,1);
+            return $self;
+        }
+
+        # check if we might have a sync word on the boundary of our buffer
+        if (substr($buf,-1,3) eq "\000\000\000") {
+            $remainder = "\000\000\000";
+        } elsif (substr($buf,-1,2) eq "\000\000") {
+            $remainder = "\000\000";
+        } elsif (substr($buf,-1,1) eq "\000") {
+            $remainder = "\000";
+        } else {
+            $remainder = undef;
+        }
+    }
+}
+
 1;
